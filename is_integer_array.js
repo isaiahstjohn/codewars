237f5733503c7c5f0b5234334d0258db7a4e6c8c
Isaiah St. John <hello@isaiah.st>
2020-01-03 00:00:36 -0500

    Is Ingeger Array? kata

---
+++ is_integer_array.js
@@ -0,0 +1,16 @@
+/*
+Write a function with the signature shown below:
+
+function isIntArray(arr) {
+  return true
+}
+returns true / True if every element in an array is an integer or a float with no decimals.
+returns true / True if array is empty.
+returns false / False for every other input.
+*/
+
+function isIntArray(arr){
+  return Array.isArray(arr) && arr.every(item => {
+    return typeof item === 'number' && Number.isInteger(item);
+  });
+}
